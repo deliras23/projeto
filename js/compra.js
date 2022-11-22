9fc49bf2a848cae4f7f6936e154b5a6c7ee6010e
Natan Lira <91631690+deliras23@users.noreply.github.com>
2022-11-22 01:36:35 -0300

    toma

--- js/compra.js
+++ js/compra.js
@@ -31,3 +31,71 @@ function add(valor,marcado) {
     //saída
     document.getElementById('total').value = total;
 }
+
+function concluir() {
+  alert("Sua compra foi concluída, verifique seu e-mail")
+}
+
+function mascara_telefone(obj) {
+  if (obj.value.length == 0) {
+    obj.value += "("
+  }
+  if (obj.value.length == 3) {
+    obj.value += ")"
+  }
+  if (obj.value.length == 9) {
+    obj.value += "-"
+  }
+  
+}
+//function cadastrar() {
+//  alert("Seu cadastro foi concluído, volte a tela inicial")
+//}
+
+//pse né amigo, chegou até aqui é pq tu tem paciência
+
+function cadastrar(form){
+  if(form.email.value == "") {
+      alert("O Campo email é obrigatório")
+      form.email.focus();
+      return false;
+  } 
+  else if(form.senha.value == "") {
+      alert("O Campo senha é obrigatório")
+      form.senha.focus();
+      return false;
+  } 
+  else if(form.nome.value == "") {
+      alert("O Campo nome é obrigatório")
+      form.nome.focus();
+      return false;
+  } 
+  else if(form.tel.value == "") {
+      alert("O Campo Telefone é obrigatório")
+      form.tel.focus();
+      return false;
+  } 
+  else if(form.endereco.value == "") {
+      alert("O Campo Endereço é obrigatório")
+      form.endereco.focus();
+      return false;
+  } 
+  else if(form.senha.value != form.senha2.value){
+      alert("As senhas não coincidem");
+      return false;
+  }
+
+}
+
+function senhas(){
+  senha1 = document.form.senha.value;
+  senha2 = document.form.senha2.value;
+
+
+  if(senha1 != senha2) {
+      document.getElementById("msg").style.display = "block";
+  }
+  else {
+      document.getElementById("msg").style.display = "none";
+  }
+}
